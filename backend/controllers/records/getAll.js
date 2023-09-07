f4be90f9878f36b6fb15a9eb5cfac53040a74780
TurtlePower <agnkvdm@gmail.com>
2023-09-07 17:34:15 +0300

    add date picker update getAll records

--- backend/controllers/records/getAll.js
+++ backend/controllers/records/getAll.js
@@ -2,10 +2,13 @@ const { endOfDay, startOfDay } = require('date-fns');
 const { Record } = require('../../models/record');
 
 const getAll = async (req, res, next) => {
+  const today = new Date();
+  const { date = today } = req.query;
+
   const records = await Record.find({
     createdAt: {
-      $gte: startOfDay(new Date()),
-      $lte: endOfDay(new Date()),
+      $gte: startOfDay(new Date(date)),
+      $lte: endOfDay(new Date(date)),
     },
   }).populate('category', 'name type');
 
